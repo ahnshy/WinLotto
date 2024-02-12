829c3b58ea2585c6d0ceb4b4137e2f248ce2bb8a
ahnshy <ahnshy@gmail.com>
2024-02-12 12:03:15 +0900

    <Bug> : Syntext Error Fixed

--- Data/WinsNumberManager.h
+++ Data/WinsNumberManager.h
@@ -3,6 +3,7 @@
 /////////////////////////////////////////////////////////////////////////
 // #20160724 Work Ahnshy :: Winning Number Manage Class
 // CWinsNumberManager class
+
 class CWinsNumberManager
 {
 public:
@@ -10,42 +11,21 @@ public:
 	~CWinsNumberManager(void);
 
 	static CWinsNumberManager*	GetInstance();
-	static void				DestroyInstance();
+	static void					DestroyInstance();
 
 	// Init
-	void					Initialize();
-	void					SetEmpty();
+	void						Initialize();
+	void						SetEmpty();
 
-	void					SaveConfig();
-	void					ReadConfig();
+	void						SaveConfig();
+	void						ReadConfig();
 
 	// Getter / Setter
-	CString					GetPath()										{			return m_DataObject.m_strPath;							}
-	void					SetPath(CString strPath)						{			m_DataObject.m_strPath		= strPath;					}
-
-	CString					GetCurrentPath()								{			return m_DataObject.m_strCurrentPath;					}
-	void					SetCurrentPath(CString strPath)					{			m_DataObject.m_strCurrentPath= strPath;					}
-
-	CString					GetFilter()										{			return m_DataObject.m_strFilter;						}
-	void					SetFilter(CString strFilter)					{			m_DataObject.m_strFilter		= strFilter;			}
-
-	int						GetFilterIndex()								{			return m_DataObject.m_nIndex;							}
-	void					SetFilterIndex(int nIndex)						{			m_DataObject.m_nIndex		=	nIndex;					}
-
-	BOOL					GetIncludeSubDirectory()						{			return m_DataObject.m_bIncludeSubDirectory;				}
-	void					SetIncludeSubDirectory(BOOL bInclude)			{			m_DataObject.m_bIncludeSubDirectory		=	bInclude;	}
 
 
 	// Static :: Path
-	static const CString	PathName;
-	static const CString	CurrentPath;
-	static const CString	IncludeSubDirectory;
-
-	// About FileFilter
-	static const CString	FileFilter;
-	static const CString	FileFilterIndex;
+	static const CString		PathName;
 
 protected:
-	SMainDlgValueObject		m_DataObject;
 	static CWinsNumberManager*	m_pWinsNumberManager;
 };
