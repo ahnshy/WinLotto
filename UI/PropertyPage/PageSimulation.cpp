80c1f2a0c654f94b63d9e224f67a4438ad7d1812
ahnshy <ahnshy@gmail.com>
2024-02-18 21:13:24 +0900

    <feat> : Draw Lotto 50 Balls and Text Number <feat> : Draw Lotto Random Color Balls.

--- UI/PropertyPage/PageSimulation.cpp
+++ UI/PropertyPage/PageSimulation.cpp
@@ -5,7 +5,7 @@
 #include "stdafx.h"
 
 #include <math.h>
-
+#include <stdlib.h>
 #include "PageSimulation.h"
 
 #ifdef _DEBUG
@@ -75,22 +75,24 @@ HBRUSH CPageSimulation::OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor)
 	return  CResizablePage::OnCtlColor(pDC, pWnd, nCtlColor);
 }
 
+COLORREF CPageSimulation::GetRandomColor()
+{
+	return RGB((BYTE)(rand() % 255), (BYTE)(rand() % 255), (BYTE)(rand() % 255));
+}
+
 void CPageSimulation::OnPaint() 
 {
 	CPaintDC dc(this);
 	CRect rc;
 	GetClientRect(&rc);
 
-	//  LOGIN DIALOG - Append Ahnshy
-	#define		COLOR_PAGE_SIMULATION_DLG_BG_RGB						RGB(5, 36, 63)
-	#define		COLOR_PAGE_SIMULATION_DLG_BG							Color(255, 5, 36, 63)
-	#define		COLOR_PAGE_SIMULATION_DLG_BG_HIGHLIGHT_CIRCLE			Color(255, 20, 77, 126)
-
-	m_gdi.DrawGradientBackGound(dc.GetSafeHdc(), rc, COLOR_PAGE_SIMULATION_DLG_BG_RGB, COLOR_PAGE_SIMULATION_DLG_BG_HIGHLIGHT_CIRCLE, 10);
-
-	
+	CString strText;
 	float fRadios = sqrt(sqrt(double(rc.Width() * rc.Height())));
-	m_gdi.DrawBall(dc.GetSafeHdc(), RectF(10, 10, fRadios, fRadios), RGB(255, 0, 0), TRUE);
+	for (INT32 nNumber = 1 ; nNumber < 50 ; nNumber++)
+	{
+		strText.Format(_T("%d"), nNumber);
+		m_gdi.DrawBall(dc.GetSafeHdc(), RectF(10 *nNumber, 10 * ((nNumber % 10) + 10), fRadios, fRadios), GetRandomColor(), strText, TRUE);
+	}
 }
 
 BOOL CPageSimulation::OnEraseBkgnd(CDC* pDC) 
@@ -99,6 +101,17 @@ BOOL CPageSimulation::OnEraseBkgnd(CDC* pDC)
 	//CRect rt;
 	//GetClientRect(&rt);  // 클라이언트 영역의 크기 계산
 	//pDC->FillSolidRect(&rt, RGB(0, 0, 0));  // 클라이언트 영역 크기만큼 흰색으로 채운다
+
+	CRect rc;
+	GetClientRect(&rc);
+
+	//  LOGIN DIALOG - Append Ahnshy
+#define		COLOR_PAGE_SIMULATION_DLG_BG_RGB						RGB(5, 36, 63)
+#define		COLOR_PAGE_SIMULATION_DLG_BG							Color(255, 5, 36, 63)
+#define		COLOR_PAGE_SIMULATION_DLG_BG_HIGHLIGHT_CIRCLE			Color(255, 20, 77, 126)
+
+	m_gdi.DrawGradientBackGound(pDC->GetSafeHdc(), rc, COLOR_PAGE_SIMULATION_DLG_BG_RGB, COLOR_PAGE_SIMULATION_DLG_BG_HIGHLIGHT_CIRCLE, 10);
+
 
 	return TRUE;  // 상위 클래스의 OnEraseBkgnd 함수를 호출하지 않아야 바꾼 배경색이 적용된다
 	// return CDialog::OnEraseBkgnd(pDC);
