2c535c4521d340c7884d43c546585ef92e9bb855
ahnshy <ahnshy@naver.com>
2024-02-18 21:13:24 +0900

    <feat> : Draw Lotto 50 Balls and Text Number <feat> : Draw Lotto Random Color Balls.

--- Helper/GdiPlusHelper.h
+++ Helper/GdiPlusHelper.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <gdiplus.h>
+#include <map>
+using namespace std;
 using namespace Gdiplus;
+
+typedef map<INT32, RectF>				MapSize;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // #20230613 :: Gdi+ helper class
@@ -24,16 +28,23 @@ public:
 	CGdiPlusHelper(void);
 	~CGdiPlusHelper(void);
 
-	BOOL				Init();
+	BOOL					Init();
 
-	INT32				DrawBall(HDC hDC, RectF& rcRect, COLORREF colorBall, BOOL bFlat);
-	INT32				DrawGradientBackGound(HDC hDC, CRect& rcRect, COLORREF colorBackGround, Color colorHighLight, INT32 nFeather);
+	INT32					DrawBall(HDC hDC, RectF& rcRect, COLORREF colorBall, CString strText, BOOL bFlat);
+	INT32					DrawGradientBackGound(HDC hDC, CRect& rcRect, COLORREF colorBackGround, Color colorHighLight, INT32 nFeather);
 	
-	INT32				SetTransform(LPCTSTR lpszPath, Bitmap** ppBitmap);
+	INT32					SetTransform(LPCTSTR lpszPath, Bitmap** ppBitmap);
 
-	int					GetEncoderClsid(const WCHAR* pszFormat, CLSID* pClsid);
+	int						GetEncoderClsid(const WCHAR* pszFormat, CLSID* pClsid);
 
 private:
-	ULONG_PTR			m_gdiToken;
+	ULONG_PTR				m_gdiToken;
+
+	Font*					m_pTextFont;
+	
+	//CArray<RectF, RectF&>	m_arrFontSize;
+	MapSize					m_mapSize;
+
+	void					MeasureFontSize();
 
 };
