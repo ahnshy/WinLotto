788d4a98a438876278f4784a2d106d111ab6b7ca
ahnshy <ahnshy@gmail.com>
2024-02-21 21:45:47 +0900

    <fix> git rebase

--- Data/BallItem.h
+++ Data/BallItem.h
@@ -21,8 +21,13 @@ public:
 	RectF&		GetRect()				{ return m_rc; }
 	COLORREF	GetColor()				{ return m_color; }
 
+<<<<<<< HEAD
 	void		SetRect(RectF rc)		{ m_rc = rc; }
 	void		SetColor(COLORREF c)	{ m_color = c; }
+=======
+	DWORD		SetRect(RectF rc)	{ m_rc = rc; }
+	COLORREF	SetColor(COLORREF c){ m_color = c; }
+>>>>>>> 01af084 (<refactor> : Adding the RectF field in the Ball Item Class to display the movement of the ball)
 
 private:
 	RectF			m_rc;
