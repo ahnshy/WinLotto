461b219580c060cc35840cd476c8338b66199f8e
ahnshy <ahnshy@naver.com>
2025-03-23 20:04:22 +0900

    <feat> : Adding STL::Set to save the list of years where the event took place

--- Data/WinsItem.h
+++ Data/WinsItem.h
@@ -4,10 +4,11 @@
 // #20240211 Work Ahnshy :: WinsItem class
 // CWinsItem class
 // Bug Report :: ahnshy@naver.com
+#include <set>
 #include <map>
 using namespace std;
 
-
+typedef set<UINT32>						SetWinsYears;
 typedef map<DWORD, INT32>				MapWinsNumber;
 
 class CWinsItem
@@ -24,6 +25,7 @@ public:
 	INT32				GetMonth()						{ return m_nMonth;	}
 	INT32				GetDay()						{ return m_nDay;	}
 	INT32				GetNumberCount()				{ return m_mapNumbers.size(); }
+	SetWinsYears&		GetYearSet()					{ return m_setYears;}
 	MapWinsNumber&		GetNumberMap()					{ return m_mapNumbers; }
 	INT32				GetSum()						{ return m_nSum; }
 	INT32				GetSumWithBonus()				{ return m_nSumWithBonus; }
@@ -44,6 +46,7 @@ private:
 	DWORD				m_dwRound;
 	CString				m_strDate;
 	MapWinsNumber		m_mapNumbers;
+	SetWinsYears		m_setYears;
 
 	INT32				m_nSum;
 	INT32				m_nSumWithBonus;
