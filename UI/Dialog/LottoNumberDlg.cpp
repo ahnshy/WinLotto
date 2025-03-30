b7068f8034f3b70bd6604525d2d76b3e27f2bc09
ahnshy <ahnshy@naver.com>
2025-03-30 12:33:22 +0900

    <feat> : Add Extract button and click button to extract random number and insert it into ListCtrl

--- UI/Dialog/LottoNumberDlg.cpp
+++ UI/Dialog/LottoNumberDlg.cpp
@@ -1,16 +1,19 @@
 #include "stdafx.h"
 #include <CommCtrl.h>
+#include <vector>
+#include <algorithm>
 #include "LottoNumberDlg.h"
 #include "../../resource.h"
 
 BEGIN_MESSAGE_MAP(CLottoNumberDlg, CDialogEx)
-    ON_WM_SIZE()
-    ON_WM_DESTROY()
+	ON_WM_SIZE()
+	ON_WM_DESTROY()
+	ON_BN_CLICKED(1002, &CLottoNumberDlg::OnBnClickedExtract) // 버튼 핸들러 연결
 END_MESSAGE_MAP()
 
 CLottoNumberDlg::CLottoNumberDlg(CWnd* pParent)
-    : CDialogEx(IDD_DIALOG_EMPTY, pParent)
-    , m_pLottoCtrl(nullptr)
+	: CDialogEx(IDD_DIALOG_EMPTY, pParent)
+	, m_pLottoCtrl(nullptr)
 {
 }
 
@@ -20,90 +23,83 @@ CLottoNumberDlg::~CLottoNumberDlg()
 
 BOOL CLottoNumberDlg::OnInitDialog()
 {
-    CDialogEx::OnInitDialog();
+	CDialogEx::OnInitDialog();
 
-    m_pLottoCtrl = new CLottoListCtrl();
-    CRect rcDummy(0, 0, 100, 100);
-	/*
-    //m_pLottoCtrl->Create(WS_CHILD | WS_VISIBLE | LVS_REPORT | LVS_OWNERDRAWFIXED, rcDummy, this, 001);
-	m_pLottoCtrl->Create(WS_CHILD | WS_VISIBLE | LVS_REPORT, rcDummy, this, 1001);
-
-    //m_pLottoCtrl->InsertColumn(0, _T("Number"), LVCFMT_LEFT, 50);
-    //for (int i = 0; i < 50; ++i)
-    //{
-    //    CString str;
-    //    str.Format(_T("%d"), i + 1);
-    //    m_pLottoCtrl->InsertItem(i, str);
-    //}
-
-	m_pLottoCtrl->InsertColumn(0, _T("Number"), LVCFMT_CENTER, 50);
-	m_pLottoCtrl->InsertColumn(1, _T("Value"), LVCFMT_CENTER, 100);
-
-	for (int i = 0; i < 50; ++i)
-	{
-		CString numStr, valueStr;
-		numStr.Format(_T("%d"), i + 1);
-		valueStr.Format(_T("%02d"), i + 1);
-
-		int index = m_pLottoCtrl->InsertItem(i, numStr);
-
-		m_pLottoCtrl->SetItemText(index, 1, valueStr);
-	}
-	*/
-
-	//m_pLottoCtrl->Create(WS_CHILD | WS_VISIBLE | LVS_REPORT | LVS_SINGLESEL | LVS_OWNERDRAWFIXED, rcDummy, this, 1001);
+	m_pLottoCtrl = new CLottoListCtrl();
+	CRect rcDummy(0, 0, 100, 100);
 	m_pLottoCtrl->Create(WS_CHILD | WS_VISIBLE | LVS_REPORT | LVS_SINGLESEL, rcDummy, this, 1001);
 	m_pLottoCtrl->SetExtendedStyle(LVS_EX_FULLROWSELECT | LVS_EX_DOUBLEBUFFER);
-
-
-	//m_font.CreatePointFont(180, DEFAULT_FONT); // 약 12~14pt짜리 더미폰트
-	//m_pLottoCtrl->SetFont(&m_font);
-
-	m_pLottoCtrl->InsertColumn(0, _T("No"), LVCFMT_LEFT, 40);
-	m_pLottoCtrl->InsertColumn(1, _T("Numbers"), LVCFMT_LEFT, 200);
-	m_pLottoCtrl->InsertColumn(2, _T("Bonus"), LVCFMT_LEFT, 40);
-
-	//// 로또번호 + 보너스 번호 삽입
-	//int nItem = m_pLottoCtrl->InsertItem(0, _T("1"));
-	//m_pLottoCtrl->SetItemText(nItem, 1, _T("11,16,19,21,27,31"));
-	//m_pLottoCtrl->SetItemText(nItem, 2, _T("30"));
-
-	//nItem = m_pLottoCtrl->InsertItem(1, _T("2"));
-	//m_pLottoCtrl->SetItemText(nItem, 1, _T("2,6,11,16,25,31"));
-	//m_pLottoCtrl->SetItemText(nItem, 2, _T("3"));
-
-	//nItem = m_pLottoCtrl->InsertItem(2, _T("3"));
-	//m_pLottoCtrl->SetItemText(nItem, 1, _T("5,7,8,12,28,33"));
-	//m_pLottoCtrl->SetItemText(nItem, 2, _T("7"));
-
 	m_pLottoCtrl->InitializeColumns();
 
-	m_pLottoCtrl->InsertLottoRow(0, _T("11,16,19,21,27,31"), _T("30"));
-	m_pLottoCtrl->InsertLottoRow(1, _T("2,6,11,16,25,31"), _T("3"));
-	m_pLottoCtrl->InsertLottoRow(2, _T("5,7,8,12,28,33"), _T("7"));
+	m_pLottoCtrl->InsertColumn(0, _T("No."), LVCFMT_LEFT, 50);
+	m_pLottoCtrl->InsertColumn(1, _T("Numbers"), LVCFMT_LEFT, 400);
+	m_pLottoCtrl->InsertColumn(2, _T("Bonus"), LVCFMT_LEFT, 50);
 
-    return TRUE;
+
+
+
+	//m_pLottoCtrl->InsertLottoRow(0, _T("11,16,19,21,27,31"), _T("30"));
+	//m_pLottoCtrl->InsertLottoRow(1, _T("2,6,11,16,25,31"), _T("3"));
+	//m_pLottoCtrl->InsertLottoRow(2, _T("5,7,8,12,28,33"), _T("7"));
+
+	m_btnExtract.Create(_T("Extract"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, CRect(0, 0, 80, 30), this, 1002);
+	m_btnExtract.SetFaceColor(RGB(255, 255, 255));
+
+	return TRUE;
 }
 
 void CLottoNumberDlg::OnSize(UINT nType, int cx, int cy)
 {
-    CDialogEx::OnSize(nType, cx, cy);
+	CDialogEx::OnSize(nType, cx, cy);
 
-    if (m_pLottoCtrl && ::IsWindow(m_pLottoCtrl->GetSafeHwnd()))
-    {
-        m_pLottoCtrl->MoveWindow(10, 10, cx - 20, cy - 20);
-    }
+	if (m_pLottoCtrl && ::IsWindow(m_pLottoCtrl->GetSafeHwnd()))
+	{
+		m_pLottoCtrl->MoveWindow(10, 50, cx - 20, cy - 60);
+	}
+
+	// 버튼 오른쪽 상단 고정
+	if (::IsWindow(m_btnExtract.GetSafeHwnd()))
+	{
+		m_btnExtract.MoveWindow(cx - 90, 10, 80, 30);
+	}
 }
 
 void CLottoNumberDlg::OnDestroy()
 {
-    if (m_pLottoCtrl)
-    {
-        if (::IsWindow(m_pLottoCtrl->GetSafeHwnd()))
-            m_pLottoCtrl->DestroyWindow();
-        delete m_pLottoCtrl;
-        m_pLottoCtrl = nullptr;
-    }
+	if (m_pLottoCtrl)
+	{
+		if (::IsWindow(m_pLottoCtrl->GetSafeHwnd()))
+			m_pLottoCtrl->DestroyWindow();
+		delete m_pLottoCtrl;
+		m_pLottoCtrl = nullptr;
+	}
 
-    CDialogEx::OnDestroy();
+	CDialogEx::OnDestroy();
+}
+
+void CLottoNumberDlg::OnBnClickedExtract()
+{
+	std::vector<int> numbers;
+	while (numbers.size() < 7)
+	{
+		int num = rand() % 45 + 1;
+		if (std::find(numbers.begin(), numbers.end(), num) == numbers.end())
+			numbers.push_back(num);
+	}
+
+	std::sort(numbers.begin(), numbers.begin() + 6);
+
+	CString mainNums, bonusNum;
+	for (int i = 0; i < 6; ++i)
+	{
+		CString numStr;
+		numStr.Format(_T("%d"), numbers[i]);
+		mainNums += numStr;
+		if (i != 5) mainNums += _T(",");
+	}
+
+	bonusNum.Format(_T("%d"), numbers[6]);
+
+	int nItem = m_pLottoCtrl->GetItemCount();
+	m_pLottoCtrl->InsertLottoRow(nItem, mainNums, bonusNum);
 }
--- UI/Dialog/LottoNumberDlg.h
+++ UI/Dialog/LottoNumberDlg.h
@@ -5,17 +5,18 @@
 class CLottoNumberDlg : public CDialogEx
 {
 public:
-    CLottoNumberDlg(CWnd* pParent = nullptr);
-    virtual ~CLottoNumberDlg();
+	CLottoNumberDlg(CWnd* pParent = nullptr);
+	virtual ~CLottoNumberDlg();
 
 protected:
-    CLottoListCtrl* m_pLottoCtrl;
-	//CFont			m_font;
+	CLottoListCtrl* m_pLottoCtrl;
+	CMFCButton m_btnExtract;
 
 protected:
-    virtual BOOL OnInitDialog();
-    afx_msg void OnSize(UINT nType, int cx, int cy);
-    afx_msg void OnDestroy();
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnDestroy();
+	afx_msg void OnBnClickedExtract();
 
-    DECLARE_MESSAGE_MAP()
+	DECLARE_MESSAGE_MAP()
 };
