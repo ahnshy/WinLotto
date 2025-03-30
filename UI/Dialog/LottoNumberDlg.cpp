3b5573cd55e76818e9e059a5f78e69d43b7fa69c
ahnshy <ahnshy@naver.com>
2025-03-30 11:08:08 +0900

    <fixed> : Change ListCtrl properties

--- UI/Dialog/LottoNumberDlg.cpp
+++ UI/Dialog/LottoNumberDlg.cpp
@@ -51,7 +51,7 @@ BOOL CLottoNumberDlg::OnInitDialog()
 	}
 	*/
 
-	//m_pLottoCtrl->Create(WS_CHILD | WS_VISIBLE | LVS_REPORT | LVS_OWNERDRAWFIXED, rcDummy, this, 1001);
+	//m_pLottoCtrl->Create(WS_CHILD | WS_VISIBLE | LVS_REPORT | LVS_SINGLESEL | LVS_OWNERDRAWFIXED, rcDummy, this, 1001);
 	m_pLottoCtrl->Create(WS_CHILD | WS_VISIBLE | LVS_REPORT | LVS_SINGLESEL, rcDummy, this, 1001);
 	m_pLottoCtrl->SetExtendedStyle(LVS_EX_FULLROWSELECT | LVS_EX_DOUBLEBUFFER);
 
@@ -59,22 +59,28 @@ BOOL CLottoNumberDlg::OnInitDialog()
 	//m_font.CreatePointFont(180, DEFAULT_FONT); // 약 12~14pt짜리 더미폰트
 	//m_pLottoCtrl->SetFont(&m_font);
 
-	m_pLottoCtrl->InsertColumn(0, _T("Game"), LVCFMT_LEFT, 50);
-	m_pLottoCtrl->InsertColumn(1, _T("Numbers"), LVCFMT_LEFT, 400);
-	m_pLottoCtrl->InsertColumn(2, _T("Bonus"), LVCFMT_LEFT, 50);
+	m_pLottoCtrl->InsertColumn(0, _T("No"), LVCFMT_LEFT, 40);
+	m_pLottoCtrl->InsertColumn(1, _T("Numbers"), LVCFMT_LEFT, 200);
+	m_pLottoCtrl->InsertColumn(2, _T("Bonus"), LVCFMT_LEFT, 40);
 
-	// 로또번호 + 보너스 번호 삽입
-	int nItem = m_pLottoCtrl->InsertItem(0, _T("1"));
-	m_pLottoCtrl->SetItemText(nItem, 1, _T("11,16,19,21,27,31"));
-	m_pLottoCtrl->SetItemText(nItem, 2, _T("30"));
+	//// 로또번호 + 보너스 번호 삽입
+	//int nItem = m_pLottoCtrl->InsertItem(0, _T("1"));
+	//m_pLottoCtrl->SetItemText(nItem, 1, _T("11,16,19,21,27,31"));
+	//m_pLottoCtrl->SetItemText(nItem, 2, _T("30"));
 
-	nItem = m_pLottoCtrl->InsertItem(1, _T("2"));
-	m_pLottoCtrl->SetItemText(nItem, 1, _T("2,6,11,16,25,31"));
-	m_pLottoCtrl->SetItemText(nItem, 2, _T("3"));
+	//nItem = m_pLottoCtrl->InsertItem(1, _T("2"));
+	//m_pLottoCtrl->SetItemText(nItem, 1, _T("2,6,11,16,25,31"));
+	//m_pLottoCtrl->SetItemText(nItem, 2, _T("3"));
 
-	nItem = m_pLottoCtrl->InsertItem(2, _T("3"));
-	m_pLottoCtrl->SetItemText(nItem, 1, _T("5,7,8,12,28,33"));
-	m_pLottoCtrl->SetItemText(nItem, 2, _T("7"));
+	//nItem = m_pLottoCtrl->InsertItem(2, _T("3"));
+	//m_pLottoCtrl->SetItemText(nItem, 1, _T("5,7,8,12,28,33"));
+	//m_pLottoCtrl->SetItemText(nItem, 2, _T("7"));
+
+	m_pLottoCtrl->InitializeColumns();
+
+	m_pLottoCtrl->InsertLottoRow(0, _T("11,16,19,21,27,31"), _T("30"));
+	m_pLottoCtrl->InsertLottoRow(1, _T("2,6,11,16,25,31"), _T("3"));
+	m_pLottoCtrl->InsertLottoRow(2, _T("5,7,8,12,28,33"), _T("7"));
 
     return TRUE;
 }
