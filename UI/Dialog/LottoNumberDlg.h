b5d539ca5fba8b9cd3b725d0d29120e106eb0512
ahnshy <ahnshy@naver.com>
2025-03-31 22:42:31 +0900

    <feat> : Add split bar between the drawn ball and machine lottery number list controls

--- UI/Dialog/LottoNumberDlg.h
+++ UI/Dialog/LottoNumberDlg.h
@@ -15,6 +15,10 @@ protected:
 	CMFCButton			m_btnExtract;
 	CMFCButton			m_btnRemoveAll;
 
+	CRect				m_rcSplitBar;
+	BOOL				m_bDragging;
+	int					m_nSplitPos;
+
 	void InitResultList();
 	void InitExtractList();
 
@@ -22,10 +26,15 @@ protected:
 
 protected:
 	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
 	afx_msg void OnSize(UINT nType, int cx, int cy);
 	afx_msg void OnDestroy();
 	afx_msg void OnBnClickedRemoveAll();
 	afx_msg void OnBnClickedExtract();
+	afx_msg BOOL OnSetCursor(CWnd* pWnd, UINT nHitTest, UINT message);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 
 	afx_msg void OnSelChangeRandomList(NMHDR* pNMHDR, LRESULT* pResult);
 
