42fed693d6a846771fb2a8a0616ef7fb78a7cb38
ahnshy <ahnshy@naver.com>
2025-04-13 21:15:00 +0900

    <feat> : Definition of trigonometric functions and pi

--- Helper/GdiPlusHelper.h
+++ Helper/GdiPlusHelper.h
@@ -1,8 +1,14 @@
 #pragma once
 #include <gdiplus.h>
 #include <map>
+#include <vector>
+#include <cmath>
 using namespace std;
 using namespace Gdiplus;
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 #define		COLOR_PAGE_SIMULATION_DLG_BG_RGB						RGB(5, 36, 63)
 //#define		COLOR_PAGE_SIMULATION_DLG_BG							Color(255, 5, 36, 63)
@@ -24,7 +30,6 @@ typedef map<INT32, RectF>				MapSize;
 //
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 class CGdiPlusHelper
 {
 // non-static
@@ -37,7 +42,6 @@ public:
 	INT32					DrawBall(HDC hDC, RectF& rcRect, COLORREF colorBall, CString strText, BOOL bFlat);
 	INT32					DrawGradientBackGound(HDC hDC, CRect& rcRect, COLORREF colorBackGround, Color colorHighLight, INT32 nFeather);
 
-
 	// Gdi+ Double Buffering Method
 	INT32					BufferDrawContainer(CRect& rcRect, Gdiplus::PointF center, float radius);
 	INT32					BufferDrawGradientBackGound(CRect& rcRect, COLORREF colorBackGround, Color colorHighLight, INT32 nFeather);
@@ -47,14 +51,14 @@ public:
 	INT32					SetTransform(LPCTSTR lpszPath, Bitmap** ppBitmap);
 
 	int						GetEncoderClsid(const WCHAR* pszFormat, CLSID* pClsid);
+	vector<PointF>			GetArcPoints(PointF center, float radius, float startDeg, float sweepDeg, int nPoints);
 
 private:
 	ULONG_PTR				m_gdiToken;
 
 	Gdiplus::Font*			m_pTextFont;
 	Bitmap*					m_pBufferBitmap;
-	
-	//CArray<RectF, RectF&>	m_arrFontSize;
+
 	MapSize					m_mapSize;
 
 	void					MeasureFontSize();
